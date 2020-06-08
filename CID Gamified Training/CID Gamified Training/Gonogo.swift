ffde2c25cf65490a0f7df2e77d924f7db26a4a88
Christine Lou <42721534+christinelou70@users.noreply.github.com>
2020-06-08 15:43:53 -0700

    timer

--- CID Gamified Training/CID Gamified Training/Gonogo.swift
+++ CID Gamified Training/CID Gamified Training/Gonogo.swift
@@ -12,9 +12,23 @@ struct Gonogo: View {
     
     @State var index = 1
     @State var points = 0
-    
+    @State var timeRemaining = 5
+    let images = [Image("tank1"), Image("tank2")]
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     var body: some View {
         VStack {
+            Text("Time Remaining: \(timeRemaining)")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .onReceive(timer) { _ in
+                        if self.timeRemaining > 0 {
+                            self.timeRemaining -= 1
+                        } else{
+                            self.timeRemaining = 5
+                            self.index = self.index + 1
+                    }
+            }
+            
             Spacer()
             Text("Go/NoGo")
                 .font(.largeTitle)
@@ -28,6 +42,7 @@ struct Gonogo: View {
                 if self.index == 2 {
                     self.points += 1
                 }
+                self.timeRemaining = 5
                 self.index = Int.random(in: 1...2)
             }) {
                 Text("Foe")
