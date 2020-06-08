b81e134c9951168af8a51f964c6f490b0b6fa109
Enya Xing <enyaxing@berkeley.edu>
2020-06-08 16:42:58 -0700

    Changed it to a slider

--- CID Gamified Training/CID Gamified Training/Question.swift
+++ CID Gamified Training/CID Gamified Training/Question.swift
@@ -9,7 +9,8 @@
 import SwiftUI
 
 struct Question: View {
-    var questions = [
+    // From: https://higginsweb.psych.columbia.edu/wp-content/uploads/2018/07/RFQ.pdf
+    let questions = [
         "Compared to most people, are you typically unable to get what you want out of life?",
         "Growing up, would you ever “cross the line” by doing things that your parents would not tolerate?",
         "How often have you accomplished things that got you \"psyched\" to work even harder?",
@@ -22,37 +23,42 @@ struct Question: View {
         "I feel like I have made progress toward being successful in my life.",
         "I have found very few hobbies or activities in my life that capture my interest or motivate me to put effort into them."
         ]
+    let responseDescription = [["never or seldom", "sometimes", "very often"],
+                               ["never or seldom", "sometimes", "very often"],
+                               ["never or seldom", "a few times", "many times"],
+                               ["never or seldom", "sometimes", "very often"],
+                               ["never or seldom", "sometimes", "always"],
+                               ["never or seldom", "sometimes", "very often"],
+                               ["never or seldom", "sometimes", "very often"],
+                               ["never or seldom", "sometimes", "very often"],
+                               ["never true", "sometimes true", "very often true"],
+                               ["certainly false", " ", "certainly true"],
+                               ["certainly false", " ", "certainly true"]
+    ]
     
     @State private var responses: [Int] = []
     @State private var questionCount = 0
-    @State private var showingAlert = false
+    @State private var curResponse = 3.0
     var body: some View {
         NavigationView {
             VStack {
+                Spacer()
                 Text(questions[questionCount])
                     .font(.headline)
                     .padding([.leading, .bottom, .trailing])
+                Spacer()
                 HStack {
-                    Button(action: {self.answered(1)}) {
-                        Text("1")
-                    }
-                    Button(action: {self.answered(2)}) {
-                        Text("2")
-                    }
-                    Button(action: {self.answered(3)}) {
-                        Text("3")
-                    }
-                    Button(action: {self.answered(4)}) {
-                        Text("4")
-                    }
-                    Button(action: {self.answered(5)}) {
-                        Text("5")
-                    }
+                    Slider(value: $curResponse, in: 1...5, step: 1)
+                    .padding([.horizontal])
                 }
+                Text("\(Int(curResponse))")
+                Text("\(getResponseDescription())")
+                Spacer()
             }
             .navigationBarTitle(Text("Quiz")
             .font(.largeTitle))
         }
+        .padding()
         
     }
     
@@ -65,6 +71,23 @@ struct Question: View {
     
     func nextQuestion() {
         questionCount += 1
+    }
+    
+    func intCurResponse() -> Int {
+        return Int(curResponse)
+    }
+    
+    func getResponseDescription() -> String {
+        let responseAsInt = intCurResponse()
+        if responseAsInt == 1 {
+            return responseDescription[questionCount][0]
+        } else if responseAsInt == 3 {
+            return responseDescription[questionCount][1]
+        } else if responseAsInt ==  5 {
+            return responseDescription[questionCount][2]
+        }
+        
+        return " "
     }
 }
 
