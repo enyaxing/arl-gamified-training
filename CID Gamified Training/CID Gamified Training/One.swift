be11a68c18f6b37182d7e27766f0dcada41a9a3f
Kyle Lui <kyle31412@gmail.com>
2020-06-12 10:11:04 -0400

    transition fix

--- CID Gamified Training/CID Gamified Training/One.swift
+++ CID Gamified Training/CID Gamified Training/One.swift
@@ -11,23 +11,19 @@ import SwiftUI
 struct One: View {
     @Binding var playing: Bool
     var body: some View {
-        ZStack {
-            VStack {
+        VStack {
+            HStack {
                 Text("Correct! Nice job!")
                     .font(.largeTitle)
                     .fontWeight(.bold)
-                    .position(x: 200, y: 100)
                 LottieView(filename: "happy", playing: $playing)
                 .frame(width: 50, height: 50)
-                .position(x: 370, y: -120)
-                HStack {
-                    LottieView(filename: "blackplus", playing: $playing)
-                    .frame(width: 400, height: 400)
-                    .position(x: 125, y: 0)
-                    LottieView(filename: "1", playing: $playing)
-                    .frame(width: 300, height: 300)
-                    .position(x: 110, y: 0)
-                }
+            }
+            HStack {
+                LottieView(filename: "blackplus", playing: $playing)
+                .frame(width: 200, height: 300)
+                LottieView(filename: "1", playing: $playing)
+                .frame(width: 200, height: 300)
             }
         }
     }
