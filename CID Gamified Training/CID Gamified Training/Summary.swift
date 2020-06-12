b93dc5d0d5f83a52d673a30ee0f0d8955bc1e8fe
Christine Lou <42721534+christinelou70@users.noreply.github.com>
2020-06-12 13:07:03 -0700

    christine

--- CID Gamified Training/CID Gamified Training/Summary.swift
+++ CID Gamified Training/CID Gamified Training/Summary.swift
@@ -18,37 +18,39 @@ struct Summary: View {
                  .font(.largeTitle)
                  .fontWeight(.bold)
                     VStack {
+                        Text("Question 1")
+                        .font(.title)
+                        .fontWeight(.semibold)
                         Image("tank2").resizable().scaledToFit()
-             HStack {
-                 Text("The correct answer is")
-                     .font(.title)
-                     .fontWeight(.semibold)
-                 Text("Friend")
-                     .font(.title)
-                     .fontWeight(.semibold)
-                     .foregroundColor(Color.green)
-                 + Text(".")
-                     .font(.title)
-                     .fontWeight(.semibold)
+                        HStack {
+                            Text("The correct answer is")
+                                .font(.title)
+                                .fontWeight(.semibold)
+                            Text("Friend")
+                                .font(.title)
+                                .fontWeight(.semibold)
+                                .foregroundColor(Color.green)
+                            + Text(".")
+                                .font(.title)
+                                .fontWeight(.semibold)
+                        }
+                        HStack {
+                            Text("You chose")
+                                .font(.title)
+                                .fontWeight(.semibold)
+                            Text("Foe")
+                                .font(.title)
+                                .fontWeight(.semibold)
+                                .foregroundColor(Color.red)
+                            + Text(".")
+                                .font(.title)
+                                .fontWeight(.semibold)
+                        }
                     }
-             HStack {
-                 Text("You chose")
-                     .font(.title)
-                     .fontWeight(.semibold)
-                 Text("Foe")
-                     .font(.title)
-                     .fontWeight(.semibold)
-                     .foregroundColor(Color.red)
-                 + Text(".")
-                     .font(.title)
-                     .fontWeight(.semibold)
-                    }
-            
                 }
             }
         }
     }
-}
 
 struct Summary_Previews: PreviewProvider {
     static var previews: some View {
