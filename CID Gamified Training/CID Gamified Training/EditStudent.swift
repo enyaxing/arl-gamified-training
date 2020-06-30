8a96bf509ceb1fd9e9fcc47a48f629795ec62a0a
Kyle Lui <kyle31412@gmail.com>
2020-06-30 13:33:26 -0400

    edit student page

--- CID Gamified Training/CID Gamified Training/EditStudent.swift
+++ CID Gamified Training/CID Gamified Training/EditStudent.swift
@@ -7,15 +7,82 @@
 //
 
 import SwiftUI
+import Firebase
 
 struct EditStudent: View {
+    @State var email = ""
+    
+    /** Reference to global user variable. */
+    @EnvironmentObject var user: GlobalUser
+    
+    /** Connection to firebase user collection. */
+    let db = Firestore.firestore().collection("users")
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            TextField("Student's Email", text: $email)
+            HStack {
+                Spacer()
+                Button(action: {
+                    self.add(email: self.email)
+                }) {
+                    Text("Add Student")
+                }
+                Spacer()
+                Button(action: {
+                    self.remove(email: self.email)
+                }) {
+                    Text("Remove Student")
+                }
+                Spacer()
+            }
+        }
+    }
+    
+    func add(email: String) {
+        let instructor = db.document(self.user.uid)
+        db.getDocuments() {(query, err) in
+            if err != nil {
+                print("Error getting docs.")
+            } else {
+                for document in query!.documents {
+                    print((document.get("user") as! String).lowercased())
+                    if email.lowercased() == (document.get("user") as! String).lowercased() {
+                        instructor.updateData([
+                            "studentname": FieldValue.arrayUnion([document.get("name") as! String]),
+                            "studentuid": FieldValue.arrayUnion([document.get("uid") as! String])
+                        ])
+                        break
+                    }
+                }
+            }
+        }
+    }
+    
+    func remove(email: String) {
+        let instructor = db.document(self.user.uid)
+        db.getDocuments() {(query, err) in
+            if err != nil {
+                print("Error getting docs.")
+            } else {
+                for document in query!.documents {
+                    if email == document.get("user") as! String {
+                        instructor.updateData([
+                            "studentname": FieldValue.arrayRemove([document.get("name") as! String]),
+                            "studentuid": FieldValue.arrayRemove([document.get("uid") as! String])
+                        ])
+                        break
+                    }
+                }
+            }
+        }
     }
 }
 
+
+
 struct EditStudent_Previews: PreviewProvider {
     static var previews: some View {
-        EditStudent()
+        EditStudent().environmentObject(GlobalUser())
     }
 }
