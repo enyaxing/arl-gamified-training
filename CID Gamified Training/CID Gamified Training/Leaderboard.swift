491d0fe4de729f06bfdfa11d15d7b04bb1839c3b
Christine Lou <christinelou70@berkeley.edu>
2020-07-07 18:13:57 -0700

    achievement pages

---
+++ CID Gamified Training/CID Gamified Training/Leaderboard.swift
@@ -0,0 +1,22 @@
+//
+//  Leaderboard.swift
+//  CID Gamified Training
+//
+//  Created by Christine Lou on 7/7/20.
+//  Copyright © 2020 Alex. All rights reserved.
+//
+
+import SwiftUI
+
+struct Leaderboard: View {
+    
+    var body: some View {
+        Text("hello World")
+    }
+}
+
+struct Leaderboard_Previews: PreviewProvider {
+    static var previews: some View {
+        Leaderboard()
+    }
+}
